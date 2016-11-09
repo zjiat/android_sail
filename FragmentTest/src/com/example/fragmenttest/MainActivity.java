ecb2a164eb5e6557443820f03388fb8ede548679
gogetter <androidkid@163.com>
2016-11-09 14:05:49 +0800

    new class added

--- FragmentTest/src/com/example/fragmenttest/MainActivity.java
+++ FragmentTest/src/com/example/fragmenttest/MainActivity.java
@@ -1,9 +1,14 @@
 package com.example.fragmenttest;
 
 import android.app.Activity;
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
 public class MainActivity extends Activity {
 
@@ -11,6 +16,19 @@ public class MainActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		Button button = (Button) findViewById(R.id.button);
+		button.setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				AnotherRightFragment fragment = new AnotherRightFragment();
+				FragmentManager fragmentManager = getFragmentManager();
+				FragmentTransaction transaction = fragmentManager.beginTransaction();
+				transaction.replace(R.id.right_layout, fragment);
+				transaction.commit();
+			}
+
+		});
 	}
 
 	@Override
